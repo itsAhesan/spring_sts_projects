e81cef67d4ac914d9782dcd42b7be72871184a68
Ahesan Chowdhury <ahesanchowdhury2016@gmail.com>
2024-07-22 22:53:14 +0530

    spring aws project first commit

---
+++ aws_demo/src/main/java/com/jony/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.jony.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+	
+	@GetMapping("/")
+	public String m1() {
+		return "Hello..";
+	}
+
+}
